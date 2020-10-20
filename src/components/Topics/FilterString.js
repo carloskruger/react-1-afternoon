031be552674f0bcb682460040b4dfd9f4dbaba74
Carlos Kruger <carloskr@yahoo.com>
2020-10-20 19:53:57 -0400

    working on filter string

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -6,7 +6,15 @@ class FilterString extends Component {
     render(){
 
         return(
-            <p>FilterString Component</p>
+            
+            <div className="puzzleBox filterStringPB">
+                <h4>Filter String</h4>
+                <span className="puzzleText"></span>
+                <input className="inputLine">
+                </input>
+                <button className="confirmationButton"></button>
+                <span className="resultsBox filterStringRB"></span>
+            </div>
         )
     }
 }
